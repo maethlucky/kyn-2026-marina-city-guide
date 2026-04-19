e02eb18a811c056cee6d22ca029cd06dd01127d8
maethlucky <123992696+maethlucky@users.noreply.github.com>
2026-04-19 07:30:57 -0700

    Implemented front-end for user submission form

--- index.js
+++ index.js
@@ -2,11 +2,16 @@ const express = require("express");
 
 const app = express();
 app.set("view engine", "ejs");
+app.use(express.json());
 app.use(express.static("public"));
 
 app.get("/", (req, res) => {
   res.render("home.ejs");
 });
+
+app.get("/submit", (req, res) => {
+  res.render("submit.ejs");
+})
 
 app.get("/api/tags", (req, res) => {
   tags = require("./data/tags.js");
@@ -16,6 +21,11 @@ app.get("/api/tags", (req, res) => {
 app.get("/api/places", (req, res) => {
   places = require("./data/places.json");
   res.json(places);
+});
+
+// Route for submitting data to the sqlite database, currently a placeholder
+app.post("/api/submit", (req, res) => {
+  res.json({ success: true, id: "placeholder" });
 });
 
 app.listen(3000, () => {
