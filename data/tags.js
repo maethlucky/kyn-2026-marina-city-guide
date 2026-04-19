b63858cdf1f7fb9654d575c2f2e38ece13018d8f
maethlucky <123992696+maethlucky@users.noreply.github.com>
2026-04-18 17:58:45 -0700

    Updated UI, added extra tags, and fixed some data

--- data/tags.js
+++ data/tags.js
@@ -1,21 +1,42 @@
 const tags = [
-    { id: 'restaurant',       label: 'Restaurant' },
-    { id: 'fast_food',        label: 'Fast Food' },
-    { id: 'cafe',             label: 'Cafe' },
-    { id: 'pub',              label: 'Pub' },
-    { id: 'park',             label: 'Park' },
-    { id: 'playground',       label: 'Playground' },
-    { id: 'fitness_centre',   label: 'Fitness' },
-    { id: 'supermarket',      label: 'Supermarket' },
-    { id: 'convenience',      label: 'Convenience Store' },
-    { id: 'library',          label: 'Library' },
+    {
+        id: 'restaurant', label: 'Restaurant',
+        subcategories: [
+            { id: 'fast_food', label: 'Fast Food' },
+            { id: 'pizza', label: 'Pizza' },
+            { id: 'burger', label: 'Burgers' },
+            { id: 'mexican', label: 'Mexican' },
+            { id: 'chinese', label: 'Chinese' },
+            { id: 'japanese', label: 'Japanese' },
+            { id: 'sushi', label: 'Sushi' },
+            { id: 'vietnamese', label: 'Vietnamese' },
+            { id: 'mediterranean', label: 'Mediterranean' },
+            { id: 'american', label: 'American' },
+            { id: 'sandwich', label: 'Sandwiches' },
+        ]
+    },
+    { id: 'cafe', label: 'Cafe' },
+    { id: 'pub', label: 'Pub' },
+    
+    { id: 'fitness_centre', label: 'Fitness' },
+    { id: 'supermarket', label: 'Supermarket' },
+    { id: 'convenience', label: 'Convenience Store' },
+    { id: 'library', label: 'Library' },
     { id: 'community_centre', label: 'Community Centre' },
-    { id: 'public_building',  label: 'Public Building' },
-    { id: 'public_bookcase',  label: 'Public Bookcase' },
-    { id: 'takeaway',         label: 'Takeaway' },
-    { id: 'delivery',         label: 'Delivery' },
-    { id: 'wheelchair',       label: 'Wheelchair Accessible' },
-    { id: 'coffee_shop',      label: 'Coffee Shop' },
+    { id: 'public_bookcase', label: 'Public Bookcase' },
+    { id: 'takeaway', label: 'Takeaway' },
+    { id: 'delivery', label: 'Delivery' },
+    { id: 'wheelchair', label: 'Wheelchair Accessible' },
+    { 
+        id: 'outdoors', label: 'Outdoors',
+        subcategories: [
+            { id: 'park', label: 'Park' },
+            { id: 'beach', label: 'Beach'},
+        ]
+    },
+    { id: 'editors_choice', label: `Editor's Choice`},
+    { id: 'hidden_gem', label: 'Hidden Gem'},
+    { id: 'local_owned', label: 'Local Owned'},
 ];
 
 module.exports = tags;
